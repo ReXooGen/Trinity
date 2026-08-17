46598ed40564073aa8e2272d0cf3596a5640fcb1
ReXooGen <ajulian.fernando123@gmail.com>
2026-08-17 07:40:22 +0700

    Bump version to v1.2.3

--- src/core/version.h
+++ src/core/version.h
@@ -5,6 +5,6 @@
 // VERSIONINFO resource (Trinity.rc) builds FILEVERSION/PRODUCTVERSION from them.
 #define TRINITY_VERSION_MAJOR 1
 #define TRINITY_VERSION_MINOR 2
-#define TRINITY_VERSION_PATCH 2
+#define TRINITY_VERSION_PATCH 3
 
-#define TRINITY_VERSION "1.2.2 (vTweak by Lian)"
+#define TRINITY_VERSION "1.2.3 (vTweak by Lian)"
